7b9fd7df4a2c4603e8fc64c8fdadcc8901f16285
erase-huz <125708683+erase-huz@users.noreply.github.com>
2023-05-05 14:41:51 +0300

    Update MainActivity.java

--- app/src/main/java/com/nautilus/demo/mywebapp/MainActivity.java
+++ app/src/main/java/com/nautilus/demo/mywebapp/MainActivity.java
@@ -24,7 +24,7 @@ public class MainActivity extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        toolbar = findViewById(R.id.toolbar);
+        toolbar = findViewById(R.id.drawer_layout);
         
 
         final DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
