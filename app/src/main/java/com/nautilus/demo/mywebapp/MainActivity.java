7b5a82e6727b49b88d3acd96a466a269ad3cbcdf
erase-huz <125708683+erase-huz@users.noreply.github.com>
2023-05-05 12:08:44 +0300

    Update MainActivity.java

--- app/src/main/java/com/nautilus/demo/mywebapp/MainActivity.java
+++ app/src/main/java/com/nautilus/demo/mywebapp/MainActivity.java
@@ -25,12 +25,10 @@ public class MainActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         toolbar = findViewById(R.id.toolbar);
-        setActionBar(toolbar);
+        
 
         final DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.setDrawerListener(toggle);
-        toggle.syncState();
+
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
