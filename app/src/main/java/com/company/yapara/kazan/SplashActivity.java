6f289607a9b685bdcb7cf27dd47248c27099e50c
erase-huz <125708683+erase-huz@users.noreply.github.com>
2023-05-08 00:29:00 +0300

    Update SplashActivity.java

--- app/src/main/java/com/company/yapara/kazan/SplashActivity.java
+++ app/src/main/java/com/company/yapara/kazan/SplashActivity.java
@@ -17,39 +17,10 @@ import java.util.HashMap;
 public class SplashActivity extends Activity {
 
 
-    // -------------------- ##############   Analytics ########### ---------------
-    private static final String PROPERTY_ID = "UA-51553171-1";
-    private Tracker tracker;
-    HashMap<TrackerName, Tracker> mTrackers = new HashMap<TrackerName, Tracker>();
-
-    public enum TrackerName {
-        APP_TRACKER, // Tracker used only in this app.
-
-    }
-
-    synchronized Tracker getTracker(TrackerName trackerId) {
-        if (!mTrackers.containsKey(trackerId)) {
-
-            GoogleAnalytics analytics = GoogleAnalytics.getInstance(this);
-            Tracker t = analytics.newTracker(PROPERTY_ID);
-            mTrackers.put(trackerId, t);
-
-        }
-        return mTrackers.get(trackerId);
-    }
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splash);
-
-        // ---------------------- ANALYTICS ---------------------
-
-        GoogleAnalytics.getInstance(this).newTracker(PROPERTY_ID);
-        GoogleAnalytics.getInstance(this).getLogger().setLogLevel(Logger.LogLevel.VERBOSE);
-        tracker = getTracker(TrackerName.APP_TRACKER);
-        tracker.setScreenName("SplashActivity");
-        tracker.send(new HitBuilders.AppViewBuilder().build());
 
 
         Thread splashThread = new Thread() {
