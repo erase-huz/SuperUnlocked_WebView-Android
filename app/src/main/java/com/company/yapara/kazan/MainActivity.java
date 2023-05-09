283b79d4099d8376dea7dea122be4cd904c848ee
erase-huz <125708683+erase-huz@users.noreply.github.com>
2023-05-09 18:26:07 +0300

    Update MainActivity.java

--- app/src/main/java/com/company/yapara/kazan/MainActivity.java
+++ app/src/main/java/com/company/yapara/kazan/MainActivity.java
@@ -14,16 +14,47 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
+
+import android.annotation.SuppressLint;
+import android.app.DownloadManager;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.net.http.SslError;
+
+import android.os.Environment;
+import android.os.Handler;
+import android.view.KeyEvent;
+import android.webkit.DownloadListener;
+import android.webkit.MimeTypeMap;
+import android.webkit.SslErrorHandler;
+import android.webkit.URLUtil;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
-
+    String url = "http://codeflarelimited.com";
+    WebView webView;
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        
+        webView = findViewById(R.id.web);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.getSettings().setSupportZoom(true);
+        webView.getSettings().setDomStorageEnabled(true);
+        webView.setWebViewClient(new myWebViewclient());
+        webView.loadUrl(url);
+        
 
         final DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
@@ -80,54 +111,20 @@ public class MainActivity extends AppCompatActivity
 
         return super.onOptionsItemSelected(item);
     }
+    
+    
+    public class myWebViewclient extends WebViewClient{
 
-    @SuppressWarnings("StatementWithEmptyBody")
-    @Override
-    public boolean onNavigationItemSelected(MenuItem item) {
-        // Handle navigation view item clicks here.
-        int id = item.getItemId();
-        Fragment fragment = null;
-
-        if (id == R.id.nav_1) {
-
-            // ---------------------------------  Load WebiView with Local URL -------------------- //
-
-            Bundle bundle = new Bundle();
-            bundle.putString("type", "file");
-            bundle.putString("url", "local_file.html");
-            fragment = new FragmentWeb();
-            fragment.setArguments(bundle);
-
-        } else if (id == R.id.nav_2) {
-
-            // ---------------------------------  Load WebiView with Remote URL -------------------- //
-            Bundle bundle = new Bundle();
-            bundle.putString("type", "url");
-            bundle.putString("url", "http://www.w3schools.com/");
-            fragment = new FragmentWeb();
-            fragment.setArguments(bundle);
-
-        } else if (id == R.id.nav_3) {
-
-            // ---------------------------------  Load WebiView with Remote URL -------------------- //
-            Bundle bundle = new Bundle();
-            bundle.putString("type", "file");
-            bundle.putString("url", "interactive.html");
-            fragment = new FragmentWebInteractive();
-            fragment.setArguments(bundle);
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            view.loadUrl(url);
+            return true;
         }
+        
 
-        FragmentManager fragmentManager = getFragmentManager();
-        fragmentManager.beginTransaction().replace(R.id.frame_container, fragment).addToBackStack(null).commit();
-
-
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        drawer.closeDrawer(GravityCompat.START);
-        return true;
+        @Override
+        public void onPageFinished(WebView view, String url) {
+            super.onPageFinished(view, url);
+        }
     }
 }
