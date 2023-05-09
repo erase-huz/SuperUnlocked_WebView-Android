e9af8eba28e00f1c27b0506d73eb1fe9b795ecb0
erase-huz <125708683+erase-huz@users.noreply.github.com>
2023-05-09 19:05:33 +0300

    Update MainActivity.java

--- app/src/main/java/com/company/yapara/kazan/MainActivity.java
+++ app/src/main/java/com/company/yapara/kazan/MainActivity.java
@@ -96,19 +96,5 @@ public class MainActivity extends AppCompatActivity {
         return true;
     }
     
-    
-    public class myWebViewclient extends WebViewClient{
-
-        @Override
-        public boolean shouldOverrideUrlLoading(WebView view, String url) {
-            view.loadUrl(url);
-            return true;
-        }
-        
-
-        @Override
-        public void onPageFinished(WebView view, String url) {
-            super.onPageFinished(view, url);
-        }
-    }
+   
 }
