7a66b70fdd3170ce5406680e23cd0c2df53f3c58
erase-huz <125708683+erase-huz@users.noreply.github.com>
2023-05-10 12:56:28 +0300

    Update MainActivity.java

--- app/src/main/java/com/company/yapara/kazan/MainActivity.java
+++ app/src/main/java/com/company/yapara/kazan/MainActivity.java
@@ -73,7 +73,7 @@ public class MainActivity extends AppCompatActivity {
                     drawer.openDrawer(GravityCompat.START);
                 }
             }
-        };
+        });
 
 
     }
